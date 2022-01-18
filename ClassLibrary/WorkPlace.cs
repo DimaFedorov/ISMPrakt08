7bd84d149eb7a961432e88916cf45c2250ad30e4
Dmytro Fedorov <dim.fed.13@gmail.com>
2022-01-18 10:03:47 +0200

    Created parameters and copy constructor for class work place

--- ClassLibrary/WorkPlace.cs
+++ ClassLibrary/WorkPlace.cs
@@ -8,5 +8,29 @@ namespace ClassLibrary
 {
     class WorkPlace
     {
+        protected string NameCompany;
+        protected Address Address;
+        protected string Specialty;
+        protected int Experience;
+        protected long PhoneNumber;
+        protected string Email;
+
+        public WorkPlace(string namecompany, Address address, string specialty, int experience, long phonenumber, string email)
+        {
+            NameCompany = namecompany;
+            Address = new Address(address);
+            Specialty = specialty;
+            Experience = experience;
+            PhoneNumber = phonenumber;
+            Email = email;
+        }
+
+        public WorkPlace(WorkPlace workPlace)
+        {
+            NameCompany = workPlace.NameCompany;
+            Address = new Address(workPlace.Address);
+            Specialty = workPlace.Specialty;
+            Experience = workPlace.Experience;
+        }
     }
 }

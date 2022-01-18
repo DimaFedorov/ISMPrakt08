292930352ed4871da49dbc540250f3519d6c837c
Dmytro Fedorov <dim.fed.13@gmail.com>
2022-01-18 10:01:19 +0200

    Created get and set for class employee

--- ClassLibrary/Employee.cs
+++ ClassLibrary/Employee.cs
@@ -38,6 +38,69 @@ namespace ClassLibrary
             DateTime cloned = toBeClonedDateTime;
             WorkPlace = new WorkPlace(employee.WorkPlace);
         }
-
+        public DateTime GetDate()
+        {
+            return Date;
+        }
+        public void SetDate(DateTime date)
+        {
+            Date = date;
+        }
+        public string GetFirstName()
+        {
+            return FirstName;
+        }
+        public string GetLastName()
+        {
+            return LastName;
+        }
+        public string GetSurname()
+        {
+            return Surname;
+        }
+        public string GetEducation()
+        {
+            return Education;
+        }
+        public string GetLanguage()
+        {
+            return Language;
+        }
+        public string GetHobby()
+        {
+            return Hobby;
+        }
+        public WorkPlace GetWorkPlace()
+        {
+            return WorkPlace;
+        }
+        public void SetFirstName(string firstname)
+        {
+            FirstName = firstname;
+        }
+        public void SetLastName(string lastname)
+        {
+            LastName = lastname;
+        }
+        public void SetSurname(string surname)
+        {
+            Surname = surname;
+        }
+        public void SetWorkPlace(WorkPlace workPlace)
+        {
+            WorkPlace = workPlace;
+        }
+        public void SetEducation(string education)
+        {
+            Education = education;
+        }
+        public void SetLanguage(string language)
+        {
+            Language = language;
+        }
+        public void SetHobby(string hobby)
+        {
+            Hobby = hobby;
+        }
     }
 }

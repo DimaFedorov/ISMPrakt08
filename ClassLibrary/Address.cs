f8db11a6224044127c63335d136e1425cf195d9c
Dmytro Fedorov <dim.fed.13@gmail.com>
2022-01-18 10:06:18 +0200

    Created parameters and copy constructor for class address

--- ClassLibrary/Address.cs
+++ ClassLibrary/Address.cs
@@ -8,5 +8,28 @@ namespace ClassLibrary
 {
     class Address
     {
+        protected string Country;
+        protected string City;
+        protected string Street;
+        protected int HouseNumber;
+        protected int Index;
+
+        public Address(string country, string city, string street, int housenumber, int index)
+        {
+            Country = country;
+            City = city;
+            Street = street;
+            HouseNumber = housenumber;
+            Index = index;
+        }
+
+        public Address(Address address)
+        {
+            Country = address.Country;
+            City = address.City;
+            Street = address.Street;
+            HouseNumber = address.HouseNumber;
+            Index = address.Index;
+        }
     }
 }
